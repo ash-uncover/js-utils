b92a5c43f9bca57554f2509da9e7a5da607ccf7e
ash-uncover <ash.uncover@gmail.com>
2020-12-06 23:28:20 +0100

    Update StringUtils.ts

--- src/StringUtils.ts
+++ src/StringUtils.ts
@@ -8,3 +8,20 @@ export const capitalize = (value: string) => {
 export const replaceAll = (value: string, replacePattern: string, replaceWith: string) => {
   return value.split(replacePattern).join(replaceWith)
 }
+
+export const easenSearch = (value: string) => {
+  let result = value.toLowerCase()
+
+  result = replaceAll(result, 'à', 'a')
+  result = replaceAll(result, 'é', 'e')
+  result = replaceAll(result, 'è', 'e')
+  result = replaceAll(result, 'ê', 'e')
+  result = replaceAll(result, 'î', 'i')
+  result = replaceAll(result, 'ï', 'i')
+  result = replaceAll(result, 'ô', 'o')
+  result = replaceAll(result, 'ö', 'o')
+  result = replaceAll(result, 'û', 'u')
+  result = replaceAll(result, 'ù', 'u')
+
+  return result
+}
