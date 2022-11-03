6b9bd25426344aa6ff3d831f68111bc1fcf48196
Antoine MAILLARD <ash.uncover@gmail.com>
2022-11-03 15:48:20 +0100

    add jsdom

--- jest.config.js
+++ jest.config.js
@@ -1,65 +1,33 @@
 module.exports = {
-
   roots: [
-
     '<rootDir>/src',
-
     '<rootDir>/test'
-
   ],
-
   transform: {
-
     '^.+\\.tsx?$': 'ts-jest'
-
   },
-
   collectCoverageFrom: [
-
     'src/**/*.{ts,tsx,js,jsx}',
-
     '!**/node_modules/**',
-
     '!**/vendor/**',
-
     '!**/index.ts',
-
     '!**/index.tsx',
-
     '!**/index.js',
-
     '!**/index.jsx'
-
   ],
-
   coverageDirectory: 'test/__coverage__',
-
   coverageThreshold: {
-
     global: {
-
       branches: 0,
-
       functions: 0,
-
       lines: 0,
-
       statements: 0
-
     }
-
   },
-
   coverageReporters: [
-
     'json',
-
     'lcov',
-
     'text',
-
     'text-summary'
-
   ]
-
 }
